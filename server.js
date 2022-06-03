336e5b3f5f5b031c9a77ef62d926bce328dcaf17
Tony Herbert <tonyherbert22@gmail.com>
2022-06-02 19:59:46 -0500

    working rapper api

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+// ! This is the API OBJECT
+const rappers = {
+    "21 savage": {
+        birthName: "Sheyaa Bin Abraham-Joseph",
+        birthLocation: "London, England",
+        age: 29,
+    },
+    "chance the rapper": {
+        birthName: "Chancelor Bennett",
+        birthLocation: "Chicago, Illinois",
+        age: 29,
+    },
+    unknown: {
+        birthName: "unknown",
+        birthLocation: "unknown",
+        age: 0,
+    },
+};
+
+app.listen(PORT, () => {
+    console.log(`Your server is running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:name", (req, res) => {
+    const rapperName = req.params.name.toLowerCase();
+    if (rappers[rapperName]) {
+        res.json(rappers[rapperName]);
+    } else {
+        res.json(rappers['unknown']);
+    }
+});
